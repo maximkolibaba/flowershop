be1557ead822e324b413f44106808f2e908a0630
Maksim Kolibaba <maksim.kolibaba@accenture.com>
2017-10-04 17:48:27 +0300

    Add rest redirect

--- src/main/java/com/accenture/flowershop/fe/rs/UserRest.java
+++ src/main/java/com/accenture/flowershop/fe/rs/UserRest.java
@@ -1,16 +1,26 @@
 package com.accenture.flowershop.fe.rs;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
 
 @Path("{username}")
 public class UserRest {
+    @Context
+    private HttpServletRequest request;
+
+    @Context
+    private HttpServletResponse response;
+
     @PathParam("username")
     private String username;
 
     @GET
-    public void info() {
-
+    public void info() throws Exception {
+        // working tipa
+        request.getRequestDispatcher("/pages/profile/info.jsp").forward(request,response);
     }
 }
